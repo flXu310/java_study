15e5e3dba9cc88dac59b5ba09d9fcf3546aad7a4
flXu001 <dgshxu666@gmail.com>
2024-04-14 17:01:00 +0800

    开始使用git管理代码

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello world!");
 
-
-
+// 项目已经交给git管理
+        System.out.println("git have use");
     }
 }
